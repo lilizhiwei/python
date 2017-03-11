971de91609c0ce5d687483a97cb2dcaaecba57ce
lizhiwei <1548790965@qq.com>
2017-03-11 19:41:13 +0800

    上传学习代码

--- python高级-代码/7，异步IO/aiohttp1.py
+++ python高级-代码/7，异步IO/aiohttp1.py
@@ -4,12 +4,12 @@ from aiohttp import web
 
 async def index(request):
     await asyncio.sleep(0.5)
-    return web.Response(body=b'<h1>Index</h1>')
+    return web.Response(body=b'<h1>Index</h1>', content_type='text/html', charset='UTF-8')
 
 async def hello(request):
     await asyncio.sleep(0.5)
     text = '<h1>hello, %s!</h1>' % request.match_info['name']
-    return web.Response(body=text.encode('utf-8'))
+    return web.Response(body=text.encode('utf-8'), content_type='text/html', charset='UTF-8')
 
 async def init(loop):
     app = web.Application(loop=loop)
